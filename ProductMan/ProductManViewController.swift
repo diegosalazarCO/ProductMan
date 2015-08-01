a9741db9534e7b7bde230b46998cb226f46aec88
Diego Salazar <kirafreaky@gmail.com>
2015-07-31 23:50:02 -0500

    Added view-controllers

---
+++ ProductMan/ProductManViewController.swift
@@ -0,0 +1,15 @@
+//
+//  ViewController.swift
+//  ProductMan
+//
+//  Created by Diego Salazar on 7/30/15.
+//  Copyright (c) 2015 Diego Salazar. All rights reserved.
+//
+
+import UIKit
+
+class ProductManViewController: UIViewController {
+
+
+}
+
